59dc8873a0f45f6952609c65aa9a5e1fa2006bfd
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-15 21:50:18 +0200

    regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ filter_datum function module
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -15,3 +16,19 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=.+?{separator}",
                          f"{field}={redaction}{separator}", f"{message}")
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return self.filter_datum(message)
