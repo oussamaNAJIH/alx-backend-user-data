b532752514e857de51e144c4c9107dc1ec468c64
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-15 21:04:13 +0200

    regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+filter_datum function module
+"""
+from typing import List
+import re
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    """
+    returns the log message obfuscated
+    """
+    pass
