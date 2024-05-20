1055183772357a78aadc2b355dfc951951d29411
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-20 17:54:50 +0200

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,7 +26,7 @@ def unauthorized(error) -> str:
     """
     unauthorized
     """
-    return jsonify({"error": "Unauthorized"})
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
