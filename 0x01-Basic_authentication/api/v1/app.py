a8b80bff6399bb55dce540cda3d0234e5dd61036
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-20 19:03:03 +0200

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -40,6 +40,21 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def before_request():
+    """
+    Filter each request
+    """
+    if auth is not None:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if not auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
