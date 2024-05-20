64b2a77fe231458116c6d82982c929d8f87ad433
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-20 19:04:52 +0200

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,10 +45,10 @@ def before_request():
     """
     Filter each request
     """
-    if auth is not None:
+    if auth:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                           '/api/v1/forbidden/']
-        if not auth.require_auth(request.path, excluded_paths):
+        if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
