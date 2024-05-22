7125d69b1a4a343cdc1bee258e86b971ff60b5a6
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-22 17:52:31 +0200

    Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ This module for SessionAuth class
 """
 from api.v1.auth.auth import Auth
 import uuid
+from api.v1.views.users import User
 
 
 class SessionAuth(Auth):
@@ -33,3 +34,10 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+    
+    def current_user(self, request=None):
+        """
+        returns a User instance based on a cookie value
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie)
+        return User.get(user_id)
